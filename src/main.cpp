c0adef5f58fa5900f5348d720b5bc0c1dc6ab503
Aleks Nevkontakte <aleks@aradmin.org.ru>
2014-06-27 22:31:13 +0700

    ADD: Added library interface API stubs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <sign_engine.h>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+Gost12S512Status Gost12S512Sign( const char* privateKey,
+                                 const char* rand,
+                                 const char* hash,
+                                 char* signature ) {
+    return kStatusInternalError;
 }
 
+Gost12S512Status Gost12S512Verify( const char* publicKeyX,
+                                   const char* publicKeyY,
+                                   const char* hash,
+                                   const char* signature ) {
+    return kStatusInternalError;
+}

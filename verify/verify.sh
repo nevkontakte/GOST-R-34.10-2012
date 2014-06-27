df935e0188dd5ca54189a9adbb4a0d02b0244cd5
Aleks Nevkontakte <aleks@aradmin.org.ru>
2014-06-27 22:37:31 +0700

    ADD: Added implementation verification scripts

---
+++ verify/verify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0);
+
+TEST_EXE=$(ls ../ext/_result/*/*/bin/signature_contest | head -n 1)
+ECC_LIBRARY="../build/libgost_ecc.so"
+
+$TEST_EXE "${ECC_LIBRARY}" "test.dat"

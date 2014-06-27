76ba54b24573cc4c71b13f95638acf2c084833f0
Aleks Nevkontakte <aleks@aradmin.org.ru>
2014-06-28 02:35:00 +0700

    ADD: Produce debug output only when DEBUG is defined

--- src/signature.cpp
+++ src/signature.cpp
@@ -43,36 +43,51 @@ signature::signature(u_int64_t (&modulus)[8], u_int64_t (&a)[8], u_int64_t (&b)[
       subgroup(import_integer(subgroupModulus)),
       basePoint(import_integer(base_x), import_integer(base_y))
 {
+#ifdef DEBUG
     std::cout << this->curve.GetField().GetModulus() << std::endl << std::endl
                  << this->curve.GetA() << std::endl << this->curve.GetB() << std::endl << std::endl
                     << this->subgroup.GetModulus() << std::endl << std::endl
                        << this->basePoint.x << std::endl << this->basePoint.y << std::endl << std::endl;
+#endif
 }
 
 Gost12S512Status signature::sign(const byte* private_key, const byte* rand, const byte* hash, byte* signature) {
     Integer alpha = import_integer<hash_size>(hash);
     Integer d = import_integer<private_key_size>(private_key);
 
+#ifdef DEBUG
     std::cout << "alpha: " << alpha << std::endl;
     std::cout << "d: " << d << std::endl;
+#endif
 
     Integer e = this->subgroup.ConvertIn(alpha);
     if (e == 0) {
         e = 1;
     }
+#ifdef DEBUG
     std::cout << "e: " << e << std::endl;
+#endif
 
     Integer k = import_integer<rand_size>(rand);
     if(k >= this->subgroup.GetModulus()) {
         return kStatusBadInput;
     }
+
+#ifdef DEBUG
     std::cout << "k: " << k << std::endl;
+#endif
 
     ECPPoint C = this->curve.ScalarMultiply(this->basePoint, k);
+
+#ifdef DEBUG
     std::cout << "x_c: " << C.x << std::endl << "y_c: " << C.y << std::endl;
+#endif
 
     Integer r = this->subgroup.ConvertIn(C.x);
+
+#ifdef DEBUG
     std::cout << "r: " << r << std::endl;
+#endif
 
     if (r == 0) {
         return kStatusBadInput;
@@ -81,10 +96,13 @@ Gost12S512Status signature::sign(const byte* private_key, const byte* rand, cons
     Integer rd = this->subgroup.Multiply(r, d);
     Integer ke = this->subgroup.Multiply(k, e);
     Integer s = this->subgroup.Add(rd, ke);
+
+#ifdef DEBUG
     std::cout << "rd: " << rd << std::endl;
     std::cout << "ke: " << ke << std::endl;
 
     std::cout << "s:  " << s << std::endl;
+#endif
 
     if (s == 0) {
         return kStatusBadInput;
@@ -93,7 +111,9 @@ Gost12S512Status signature::sign(const byte* private_key, const byte* rand, cons
     export_integer<signature_size / 2>(r, signature);
     export_integer<signature_size / 2>(s, signature + signature_size / 2);
 
+#ifdef DEBUG
     std::cout << "Done!" << std::endl;
+#endif
 
     return kStatusOk;
 }

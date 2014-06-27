f6386a7777b6c34a2d9a4af756e4106fd5078750
Aleks Nevkontakte <aleks@aradmin.org.ru>
2014-06-28 02:22:59 +0700

    ADD: Debugging with reference example from GOST

--- src/signature.cpp
+++ src/signature.cpp
@@ -43,34 +43,48 @@ signature::signature(u_int64_t (&modulus)[8], u_int64_t (&a)[8], u_int64_t (&b)[
       subgroup(import_integer(subgroupModulus)),
       basePoint(import_integer(base_x), import_integer(base_y))
 {
+    std::cout << this->curve.GetField().GetModulus() << std::endl << std::endl
+                 << this->curve.GetA() << std::endl << this->curve.GetB() << std::endl << std::endl
+                    << this->subgroup.GetModulus() << std::endl << std::endl
+                       << this->basePoint.x << std::endl << this->basePoint.y << std::endl << std::endl;
 }
 
 Gost12S512Status signature::sign(const byte* private_key, const byte* rand, const byte* hash, byte* signature) {
     Integer alpha = import_integer<hash_size>(hash);
     Integer d = import_integer<private_key_size>(private_key);
 
+    std::cout << "alpha: " << alpha << std::endl;
+    std::cout << "d: " << d << std::endl;
+
     Integer e = this->subgroup.ConvertIn(alpha);
     if (e == 0) {
         e = 1;
     }
+    std::cout << "e: " << e << std::endl;
 
     Integer k = import_integer<rand_size>(rand);
     if(k >= this->subgroup.GetModulus()) {
         return kStatusBadInput;
     }
+    std::cout << "k: " << k << std::endl;
 
     ECPPoint C = this->curve.ScalarMultiply(this->basePoint, k);
+    std::cout << "x_c: " << C.x << std::endl << "y_c: " << C.y << std::endl;
 
     Integer r = this->subgroup.ConvertIn(C.x);
+    std::cout << "r: " << r << std::endl;
 
     if (r == 0) {
         return kStatusBadInput;
     }
 
-    Integer s = this->subgroup.Add(
-                this->subgroup.Multiply(r, d),
-                this->subgroup.Multiply(k, e)
-                );
+    Integer rd = this->subgroup.Multiply(r, d);
+    Integer ke = this->subgroup.Multiply(k, e);
+    Integer s = this->subgroup.Add(rd, ke);
+    std::cout << "rd: " << rd << std::endl;
+    std::cout << "ke: " << ke << std::endl;
+
+    std::cout << "s:  " << s << std::endl;
 
     if (s == 0) {
         return kStatusBadInput;
--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,48 @@
+#include <curve.h>
+#include <signature.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace gost_ecc;
+
+const uint64_t d[8] =
+{0x1D19CE9891EC3B28,
+ 0x1B60961F49397EEE,
+ 0x10ED359DD39A72C1,
+ 0x7A929ADE789BB9BE,
+ 0x0000000000000000,
+ 0x0000000000000000,
+ 0x0000000000000000,
+ 0x0000000000000000};
+
+const uint64_t alpha[8] =
+{0x67ECE6672B043EE5,
+ 0xCE52032AB1022E8E,
+ 0x88C09C52E0EEC61F,
+ 0x2DFBC1B372D89A11,
+ 0x0000000000000000,
+ 0x0000000000000000,
+ 0x0000000000000000,
+ 0x0000000000000000};
+
+const uint64_t rnd[8] =
+{0x4FED924594DCEAB3,
+ 0x6DE33814E95B7FE6,
+ 0x2823C8CF6FCC7B95,
+ 0x77105C9B20BCD312,
+ 0x0000000000000000,
+ 0x0000000000000000,
+ 0x0000000000000000,
+ 0x0000000000000000};
+
+inline const byte* to_bytes(const uint64_t (&data)[8]) {
+    return reinterpret_cast<const byte*>(&data);
+}
+
 int main() {
-    return 0;
+    signature s(p, a, b, q, x, y);
+    byte result[64*2];
+    std::cout << ">> Signing << " << std::endl;
+    s.sign(to_bytes(d), to_bytes(rnd), to_bytes(alpha), result);
 }

2cbb43adb157993b01e871215d6e143e9dd54bba
Aleks Nevkontakte <aleks@aradmin.org.ru>
2014-07-14 19:57:57 +0700

    ADD: Implemented NAF multiplication with interleaving

--- include/elliptic_curve.h
+++ include/elliptic_curve.h
@@ -127,6 +127,123 @@ public:
         return result;
     }
 
+    /**
+     * @brief Point addition for mixed Jacobian-affine coordinates.
+     *
+     * 8M + 3S
+     * See: Hankerson, D., Vanstone, S., & Menezes, A. (2004). Guide to elliptic curve cryptography.
+     * Page 91, alg. 3.22.
+     * @param left
+     * @param right
+     * @return
+     */
+    jacobian_point add(const jacobian_point& left, const point& right) const {
+        if (!this->allow_jacobian) {
+            throw std::invalid_argument("Parameter a for curve must be -3");
+        }
+
+        if (left == jacobian_point::inf) {
+            return jacobian_point(right);
+        } else if (right == point::inf) {
+            return left;
+        }
+
+        const field_type& f = this->field;
+
+        jacobian_point result;
+        integer_type t1, t2, t3, t4;
+
+        t1 = f.mul(left.z, left.z);
+        t2 = f.mul(t1, left.z);
+        t1 = f.mul(t1, right.x);
+        t2 = f.mul(t2, right.y);
+        t1 = f.sub(t1, left.x);
+        t2 = f.sub(t2, left.y);
+
+        if (t1 == 0) {
+            if (t2 == 0) {
+                return this->twice(jacobian_point(right));
+            } else {
+                return jacobian_point::inf;
+            }
+        }
+
+        result.z    = f.mul(left.z, t1);
+        t3          = f.mul(t1, t1);
+        t4          = f.mul(t3, t1);
+        t3          = f.mul(t3, left.x);
+        t1          = f.mul2(t3);
+        result.x    = f.mul(t2, t2);
+        result.x    = f.sub(result.x, t1);
+        result.x    = f.sub(result.x, t4);
+        t3          = f.sub(t3, result.x);
+        t3          = f.mul(t3, t2);
+        t4          = f.mul(t4, left.y);
+        result.y    = f.sub(t3, t4);
+
+        return result;
+    }
+
+    /**
+     * @brief See: http://en.wikibooks.org/wiki/Cryptography/Prime_Curve/Jacobian_Coordinates
+     *
+     * 12M + 4S
+     * @param left
+     * @param right
+     * @return
+     */
+    jacobian_point add(const jacobian_point &left, const jacobian_point &right) const {
+        if (left == jacobian_point::inf) {
+            return right;
+        } else if (right == jacobian_point::inf) {
+            return left;
+        }
+
+        const field_type& f = this->field;
+
+        jacobian_point result;
+
+        integer_type u1, u2, s1, s2, h, r, left_z_squared, right_z_squared, h_squared;
+
+        left_z_squared = f.mul(left.z, left.z);
+        right_z_squared = f.mul(right.z, right.z);
+
+        u1 = f.mul(left.x, right_z_squared);
+        u2 = f.mul(right.x, left_z_squared);
+
+        left_z_squared = f.mul(left_z_squared, left.z);
+        right_z_squared = f.mul(right_z_squared, right.z);
+
+        s1 = f.mul(left.y, right_z_squared);
+        s2 = f.mul(right.y, left_z_squared);
+
+        if (u1 == u2) {
+            if (s1 != s2) {
+                return jacobian_point::inf;
+            } else {
+                return this->twice(left);
+            }
+        }
+
+        h = f.sub(u2, u1);
+        r = f.sub(s2, s1);
+
+        result.z = f.mul(h, left.z, right.z);
+        h_squared = f.mul(h, h);
+
+        result.y = f.mul(u1, h_squared);
+        result.x = f.mul2(result.y);
+
+        h_squared = f.mul(h, h_squared);
+
+        result.x = f.sub(f.mul(r, r), h_squared, result.x);
+        result.y = f.sub(result.y, result.x);
+        result.y = f.mul(r, result.y);
+        result.y = f.sub(result.y, f.mul(s1, h_squared));
+
+        return result;
+    }
+
     template<typename P1, typename P2>
     P1 sub(const P1& left, const P2& right) const {
         return this->add(left, this->negate(right));
@@ -189,62 +306,6 @@ public:
         return result;
     }
 
-    /**
-     * @brief Point addition for mixed Jacobian-affine coordinates.
-     *
-     * 8M + 3S
-     * See: Hankerson, D., Vanstone, S., & Menezes, A. (2004). Guide to elliptic curve cryptography.
-     * Page 91, alg. 3.22.
-     * @param left
-     * @param right
-     * @return
-     */
-    jacobian_point add(const jacobian_point& left, const point& right) const {
-        if (!this->allow_jacobian) {
-            throw std::invalid_argument("Parameter a for curve must be -3");
-        }
-
-        if (left == jacobian_point::inf) {
-            return jacobian_point(right);
-        } else if (right == point::inf) {
-            return left;
-        }
-
-        const field_type& f = this->field;
-
-        jacobian_point result;
-        integer_type t1, t2, t3, t4;
-
-        t1 = f.mul(left.z, left.z);
-        t2 = f.mul(t1, left.z);
-        t1 = f.mul(t1, right.x);
-        t2 = f.mul(t2, right.y);
-        t1 = f.sub(t1, left.x);
-        t2 = f.sub(t2, left.y);
-
-        if (t1 == 0) {
-            if (t2 == 0) {
-                return this->twice(jacobian_point(right));
-            } else {
-                return jacobian_point::inf;
-            }
-        }
-
-        result.z    = f.mul(left.z, t1);
-        t3          = f.mul(t1, t1);
-        t4          = f.mul(t3, t1);
-        t3          = f.mul(t3, left.x);
-        t1          = f.mul2(t3);
-        result.x    = f.mul(t2, t2);
-        result.x    = f.sub(result.x, t1);
-        result.x    = f.sub(result.x, t4);
-        t3          = f.sub(t3, result.x);
-        t3          = f.mul(t3, t2);
-        t4          = f.mul(t4, left.y);
-        result.y    = f.sub(t3, t4);
-
-        return result;
-    }
 
     /**
      * @brief Repeated doubling algorithm.
@@ -315,21 +376,21 @@ public:
         return result.to_affine(*this);
     }
 
-    template<unsigned window = 8>
-    void comb_precompute(const point& base, point (&table)[1 << window]) const {
-        const unsigned d = field_type::bits / window + ((field_type::bits % window) ? 1 : 0);
+    template<unsigned win_left = 8>
+    void comb_precompute(const point& base, point (&table)[1 << win_left]) const {
+        const unsigned d = field_type::bits / win_left + ((field_type::bits % win_left) ? 1 : 0);
 
-        point pow2[window];
+        point pow2[win_left];
         pow2[0] = base;
 
-        for (unsigned i = 1; i < window; i++) {
+        for (unsigned i = 1; i < win_left; i++) {
             pow2[i] = this->repeated_twice(jacobian_point(pow2[i-1]), d).to_affine(*this);
         }
 
-        for (unsigned i = 0; i < (1 << window); i++) {
+        for (unsigned i = 0; i < (1 << win_left); i++) {
             jacobian_point p = jacobian_point::inf;
 
-            for (unsigned offset = 0; offset < window; offset++) {
+            for (unsigned offset = 0; offset < win_left; offset++) {
                 if ((i & (1 << offset)) != 0) {
                     p = this->add(p, pow2[offset]);
                 }
@@ -339,14 +400,14 @@ public:
         }
     }
 
-    template<unsigned window = 8>
-    point mul_scalar(const point (&comb_table)[1 << window], integer_type multiplier) const {
-        const unsigned d = field_type::bits / window + ((field_type::bits % window) ? 1 : 0);
+    template<unsigned win_left = 8>
+    point mul_scalar(const point (&comb_table)[1 << win_left], integer_type multiplier) const {
+        const unsigned d = field_type::bits / win_left + ((field_type::bits % win_left) ? 1 : 0);
 
         integer_type mask = static_cast<integer_type>((double_integer_type(1) << d) - 1);
 
-        integer_type chunks[window];
-        for (unsigned i = 0; i < window; i++) {
+        integer_type chunks[win_left];
+        for (unsigned i = 0; i < win_left; i++) {
             chunks[i] = multiplier & mask;
             multiplier >>= d;
         }
@@ -357,7 +418,7 @@ public:
             result = this->twice(result);
 
             unsigned key = 0;
-            for (unsigned j = 0; j < window; j++) {
+            for (unsigned j = 0; j < win_left; j++) {
                 key += mp::bit_test(chunks[j], i-1) << j;
             }
 
@@ -367,24 +428,24 @@ public:
         return result.to_affine(*this);
     }
 
-    template<unsigned window = 4>
-    void naf_precompute(const point& base, jacobian_point (&table)[1 << (window - 2)]) const {
+    template<unsigned win_left = 4>
+    void naf_precompute(const point& base, jacobian_point (&table)[1 << (win_left - 2)]) const {
         jacobian_point base_doubled = this->twice(jacobian_point(base));
         table[0] = jacobian_point(base);
 
-        for (unsigned i = 3; i < (1 << (window - 1)); i += 2) {
+        for (unsigned i = 3; i < (1 << (win_left - 1)); i += 2) {
             unsigned index = i / 2;
             table[index] = this->add(base_doubled, table[index - 1]);
         }
 
     }
 
-    template<unsigned window = 4>
-    jacobian_point mul_scalar(jacobian_point (&p)[1 << (window - 2)], const integer_type& multiplier) const {
+    template<unsigned win_left = 4>
+    jacobian_point mul_scalar(jacobian_point (&p)[1 << (win_left - 2)], const integer_type& multiplier) const {
         jacobian_point result = jacobian_point::inf;
 
         short naf_table[field_type::bits + 1];
-        unsigned naf_length = naf<window, integer_type>(multiplier, naf_table);
+        unsigned naf_length = naf<win_left, integer_type>(multiplier, naf_table);
 
         for (unsigned i = naf_length; i > 0; i--) {
             result = this->twice(result);
@@ -402,65 +463,47 @@ public:
         return result;
     }
 
-    /**
-     * @brief See: http://en.wikibooks.org/wiki/Cryptography/Prime_Curve/Jacobian_Coordinates
-     *
-     * 12M + 4S
-     * @param left
-     * @param right
-     * @return
-     */
-    jacobian_point add(const jacobian_point &left, const jacobian_point &right) const {
-        if (left == jacobian_point::inf) {
-            return right;
-        } else if (right == jacobian_point::inf) {
-            return left;
-        }
+    template<unsigned win_left = 4, unsigned win_right = 4>
+    jacobian_point add_mul(
+            jacobian_point (&left)[1 << (win_left - 2)], const integer_type& mul_left,
+            jacobian_point (&right)[1 << (win_right - 2)], const integer_type& mul_right
+    ) const {
+        jacobian_point result = jacobian_point::inf;
 
-        const field_type& f = this->field;
+        short naf_table_left[field_type::bits + 1];
+        short naf_table_right[field_type::bits + 1];
 
-        jacobian_point result;
+        std::fill(std::begin(naf_table_left), std::end(naf_table_left), 0);
+        std::fill(std::begin(naf_table_right), std::end(naf_table_right), 0);
 
-        integer_type u1, u2, s1, s2, h, r, left_z_squared, right_z_squared, h_squared;
+        unsigned naf_length = naf<win_left, integer_type>(mul_left, naf_table_left);
+        naf_length = std::max(naf<win_right, integer_type>(mul_right, naf_table_right));
 
-        left_z_squared = f.mul(left.z, left.z);
-        right_z_squared = f.mul(right.z, right.z);
+        for (unsigned i = naf_length; i > 0; i--) {
+            result = this->twice(result);
 
-        u1 = f.mul(left.x, right_z_squared);
-        u2 = f.mul(right.x, left_z_squared);
+            short ki = naf_table_left[i-1];
+            if (ki != 0) {
+                if (ki > 0) {
+                    result = this->add(result, left[ki/2]);
+                } else {
+                    result = this->sub(result, left[-ki/2]);
+                }
+            }
 
-        left_z_squared = f.mul(left_z_squared, left.z);
-        right_z_squared = f.mul(right_z_squared, right.z);
-
-        s1 = f.mul(left.y, right_z_squared);
-        s2 = f.mul(right.y, left_z_squared);
-
-        if (u1 == u2) {
-            if (s1 != s2) {
-                return jacobian_point::inf;
-            } else {
-                return this->twice(left);
+            ki = naf_table_right[i-1];
+            if (ki != 0) {
+                if (ki > 0) {
+                    result = this->add(result, right[ki/2]);
+                } else {
+                    result = this->sub(result, right[-ki/2]);
+                }
             }
         }
 
-        h = f.sub(u2, u1);
-        r = f.sub(s2, s1);
-
-        result.z = f.mul(h, left.z, right.z);
-        h_squared = f.mul(h, h);
-
-        result.y = f.mul(u1, h_squared);
-        result.x = f.mul2(result.y);
-
-        h_squared = f.mul(h, h_squared);
-
-        result.x = f.sub(f.mul(r, r), h_squared, result.x);
-        result.y = f.sub(result.y, result.x);
-        result.y = f.mul(r, result.y);
-        result.y = f.sub(result.y, f.mul(s1, h_squared));
-
         return result;
     }
+
 
     friend std::ostream& operator<<(std::ostream& out, const point& p) {
         if (p == point::inf) {
